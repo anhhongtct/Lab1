10f7d533c9cb530a347c10588b5b305ed405f991
anhhongtct <55115005+anhhongtct@users.noreply.github.com>
2019-09-16 20:09:48 -0400

    Lab2

--- app/src/main/java/com/example/fall2019/MainActivity.java
+++ app/src/main/java/com/example/fall2019/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
