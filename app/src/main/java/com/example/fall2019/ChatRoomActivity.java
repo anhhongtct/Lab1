a4a7f41d316eabfe8e502f3bb5bc649b06c8cdef
anhhongtct <55115005+anhhongtct@users.noreply.github.com>
2019-10-23 18:41:45 -0400

    Finish Lab5

--- app/src/main/java/com/example/fall2019/ChatRoomActivity.java
+++ app/src/main/java/com/example/fall2019/ChatRoomActivity.java
@@ -28,14 +28,17 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity{
+    public static final String ACTIVITY_NAME = "ChatRoomActivity";
     ArrayList<Message> message = new ArrayList<>();
     BaseAdapter myAdapter;
+    //Get the database
+    MyDatabaseOpenHelper opener;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
-
 
         //List view
         ListView theList =findViewById(R.id.theList);
@@ -49,14 +52,9 @@ public class ChatRoomActivity extends AppCompatActivity{
         //Chat text
         EditText chatText = findViewById(R.id.edit_chat);
 
-
-        //Get the database
-        MyDatabaseOpenHelper opener = new MyDatabaseOpenHelper(this);
-        SQLiteDatabase db =  opener.getWritableDatabase();
-
-        //query all the results from the database:
         String [] columns = {MyDatabaseOpenHelper.COL_ID, MyDatabaseOpenHelper.COL_MESSAGE, MyDatabaseOpenHelper.COL_SEND_OR_REC};
-        Cursor results = db.query(false, MyDatabaseOpenHelper.TABLE_NAME, columns, null, null, null, null, null, null);
+
+        Cursor results = runQuery(columns);
 
         //find the column indices:
         int messageColumnIndex = results.getColumnIndex(MyDatabaseOpenHelper.COL_MESSAGE);
@@ -76,11 +74,16 @@ public class ChatRoomActivity extends AppCompatActivity{
              myAdapter.notifyDataSetChanged();
         }
 
+        Cursor debug = runQuery(columns);
+        printCursor(debug);
+
+        // Create ContentValues object to write into database
         ContentValues newValues = new ContentValues();
+
         sendButton.setOnClickListener(clk -> {
             if(!TextUtils.isEmpty(chatText.getText())) {
                 newValues.put(MyDatabaseOpenHelper.COL_MESSAGE, chatText.getText().toString());
-                newValues.put(MyDatabaseOpenHelper.COL_SEND_OR_REC, true);
+                newValues.put(MyDatabaseOpenHelper.COL_SEND_OR_REC, "true");
                 long id = db.insert(MyDatabaseOpenHelper.TABLE_NAME,MyDatabaseOpenHelper.COL_MESSAGE, newValues);
                 message.add(new Message(chatText.getText().toString(), true, id));
                 myAdapter.notifyDataSetChanged();
@@ -96,7 +99,7 @@ public class ChatRoomActivity extends AppCompatActivity{
         recieveButton.setOnClickListener(clk -> {
             if (!TextUtils.isEmpty(chatText.getText())) {
                 newValues.put(MyDatabaseOpenHelper.COL_MESSAGE, chatText.getText().toString());
-                newValues.put(MyDatabaseOpenHelper.COL_SEND_OR_REC, false);
+                newValues.put(MyDatabaseOpenHelper.COL_SEND_OR_REC, "false");
                 long id = db.insert(MyDatabaseOpenHelper.TABLE_NAME, MyDatabaseOpenHelper.COL_MESSAGE, newValues);
                 message.add(new Message(chatText.getText().toString(), false, id));
                 myAdapter.notifyDataSetChanged();
@@ -109,6 +112,34 @@ public class ChatRoomActivity extends AppCompatActivity{
             }
         });
 
+
+    }
+
+    public Cursor runQuery (String[] columns) {
+        //query all the results from the database:
+        opener = new MyDatabaseOpenHelper(this);
+        db = opener.getWritableDatabase();
+        Cursor results = db.query(false, MyDatabaseOpenHelper.TABLE_NAME, columns, null, null, null, null, null, null);
+        return results;
+    }
+
+    public void printCursor(Cursor c) {
+
+        Log.e(ACTIVITY_NAME, MyDatabaseOpenHelper.DATABASE_NAME);
+        Log.e(ACTIVITY_NAME, "Number of columns: " + c.getColumnCount());
+        String tableName = "";
+        String [] res = c.getColumnNames();
+        for (int i = 0; i < res.length; i++) {
+              tableName += String.format("%s", res[i]) + ", ";
+        }
+        Log.e(ACTIVITY_NAME, "Name of Columns:" + tableName);
+        Log.e(ACTIVITY_NAME, "Number of results: " + c.getCount());
+        while(c.moveToNext()) {
+            String mess = c.getString(1);
+            String send = c.getString(2);
+            Log.e(ACTIVITY_NAME, "Row: " + c.getPosition() + " Message: " + mess + " Send: " + send);
+
+        }
 
     }
 
